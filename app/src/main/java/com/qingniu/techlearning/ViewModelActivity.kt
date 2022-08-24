8004cefb16625eea63698d93b2c11826972cc958
HYR <huangyiran@yolanda.hk>
2022-08-24 18:29:25 +0800

    add viewmodel activity

---
+++ app/src/main/java/com/qingniu/techlearning/ViewModelActivity.kt
@@ -0,0 +1,12 @@
+package com.qingniu.techlearning
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *@author: hyr
+ *@date: 2022/8/24 18:21
+ *@desc:
+ */
+class ViewModelActivity : AppCompatActivity(){
+    
+}
